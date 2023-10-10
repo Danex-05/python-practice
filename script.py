93342aec5ecf7f711e5fb21f568830f1039072c6
Daniel <ayomidedaniel1212@gmail.com>
2023-10-10 11:42:35 -0400

    second commit

---
+++ script.py
@@ -0,0 +1,24 @@
+#! /usr/bin/python3
+
+import os, shutil
+
+ext = input("Input the extention you want to sort: \n")
+path = "../my_class-exercise/folders/"
+
+files = os.listdir(path)
+for file in files:
+    if file.endswith(ext):
+        if not os.path.exists(path + ext):
+            os.mkdir(path + ext)
+        if os.path.exists(path + ext):
+            new_file_path = os.path.join(path, ext)
+            source = os.path.join(path, file)
+            destination = os.path.join(new_file_path, file)
+            
+            shutil.copy(source, destination)
+            print("file copied successfully")
+        
+
+
+
+
